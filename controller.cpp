93cc03bb19ffbef23c9c5bf033b2b06068da6a1f
Alberto Formaggio <alberto.formaggio@studenti.unipd.it>
2021-01-01 16:04:29 +0100

    Corretto GetStations

--- controller.cpp
+++ controller.cpp
@@ -26,11 +26,10 @@ void Controller::GetStations(string line_descr)
 
 	string station_name;
 	lines_file >> station_name;
-	StationType station_type = StationType::kMainStation;
 	int distance = 0;
 
 	//Suppongo la prima stazione come principale, Ha distanza 0 dall'origine. Nel file è solo presente il nome
-	stations_.push_back(Station(station_name, station_type, distance));
+	stations_.push_back(MainStation(station_name, distance));
 
 	string line;
 	while (!lines_file.eof())
@@ -39,19 +38,15 @@ void Controller::GetStations(string line_descr)
 		getline(lines_file, line);
 		distance = stoi(line.substr(line.size() - 1, 1));	//leggo l'ultimo numero della riga e lo converto in intero ===> è la distanza
 		int station_type_number = stoi(line.substr(line.size() - 3, 1));
-		station_type = static_cast<StationType>(station_type_number);		//StationType è stato configurato con principale = 0 e locale = 1, un cast associa i valori corretti
 		station_name = line.substr(0, line.size() - 4);
+		
+		if (station_type_number == 0)
+			MainStation st(station_name, distance);
+		else
+			LocalStation st(station_name, distance);
 
-		Station st(station_name, station_type, distance);
 		stations_.push_back(st);
 	}
 
 	lines_file.close();
 }
-
-//DA inserire In STATION.H !!!!!!!!!!!!!!!!!!!!!!!!!!
-enum class StationType
-{
-	kMainStation = 0,
-	kLocalStation = 1,
-};
