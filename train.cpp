a63ee3fdc8788a3fa5f8d518827b82934e0a274a
ANNA SCARPA FALCE <anna.scarpafalce@studenti.unipd.it>
2021-01-06 17:22:06 +0100

    aggiunta train.cpp

---
+++ train.cpp
@@ -0,0 +1,26 @@
+/*
+	@author Anna Scarpa Falce
+*/
+
+#include "train.h"
+
+void Train::setAverageSpeed(Station& from, const Station& to, int time_leaving, int& time_arrival, int delay_time, int max_speed)
+{
+	if (time_leaving < 0)
+		throw InvalidTime();
+	int distance = abs(from.kDistanceFromOrigin - to.kDistanceFromOrigin)-2*5; //funzione per avere dist da parcheggio(5);
+	if (time_arrival < time_leaving)
+		time_arrival = (distance / max_speed) + delay_time;
+	int time = (time_arrival - time_leaving - getDelay() - 2*5 / from.getStandardPlatform().slowSpeed);
+	setSpeed(distance / time );
+	if (getSpeed() > max_speed)
+		setSpeed(max_speed);
+}
+void Train::editDelay(int d)
+{
+	setDelay(getDelay() + d);
+}
+void Train::leaving(Platform& pl)
+{
+	pl.free();
+}
