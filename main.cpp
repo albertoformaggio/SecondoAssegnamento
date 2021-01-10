f986dfe47946201e96127dd23575a20ec9da1c0e
ANNA SCARPA FALCE <anna.scarpafalce@studenti.unipd.it>
2021-01-10 18:53:42 +0100

    main sistemto ops

--- main.cpp
+++ main.cpp
@@ -3,9 +3,5 @@
 int main()
 {
 	Controller c("line_description.txt","timetables.txt");
-	//c.printEvents();
-	int speed = 240;
-	int distance = 15;			//distanza richiesta-parcheggio
-	int time = static_cast<int>(round(static_cast<double>(distance) / speed * 60));
-	std::cout << time;
+	c.printEvents();
 }
