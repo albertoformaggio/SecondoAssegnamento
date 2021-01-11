e109829d563ebb43f1eda905da9eebcbcfafacb1
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2021-01-11 11:41:12 +0100

    Fix mainstatio.h

--- mainStation.h
+++ mainStation.h
@@ -8,7 +8,7 @@
 class mainStation : public Station {
 public:
 	mainStation(int distance, std::string name);
-	transitPlatform getTransitPlatform(bool direction) override { throw std::exception(); }
+	Platform* getTransitPlatform(bool direction) override { throw std::exception(); }
 };
 
 #endif
