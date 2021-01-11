10d577f267f4743b69f1c4b37c542d232f94ccd3
Alberto Formaggio <alberto.formaggio@studenti.unipd.it>
2021-01-11 21:14:36 +0100

    Finito platform request

--- controller2.cpp
+++ controller2.cpp
@@ -1,6 +1,8 @@
 /* @author Michele Rieppi */
 
 #include "controller.h"
+//AGGIUNGI TUTTI I GETDELAY AD i->GetTime() e cur->GetTime()
+
 void Controller::handlePlatformRequest(std::vector<Event>::iterator cur) { //richiesta di un binario alla stazione successiva (che so già)
 
 	const int minPerHours = 60;
@@ -28,7 +30,7 @@ void Controller::handlePlatformRequest(std::vector<Event>::iterator cur) { //ric
 				events_.push_back(park);
 			}
 		}
-		else {
+		else {																								//Altrimenti transito fino alla prossima stazione, devo capire quando farò richiesta e adatterò la mia velocità
 			Station* next_station = GetNextStation(cur->GetStation(), cur->GetTrain());						//Trovo la prossima stazione
 			Event* last_event = nullptr;
 			for (auto i = cur - 1; i >= events_.begin(); i--)												//Ciclo sugli eventi
@@ -54,11 +56,24 @@ void Controller::handlePlatformRequest(std::vector<Event>::iterator cur) { //ric
 				}
 			}
 			if (last_event != nullptr) {
+				int next_station_distance = next_station->kDistanceFromOrigin;
+				int arrive_time = -1;
+				if (dynamic_cast<mainStation*>(next_station) == nullptr)
+					next_station_distance += 5;	//Se la prossima stazione è locale, arrivo alla velocità massima al punto di fermata del treno, non devo quindi tenere conto dei 5 chilometri al parcheggio (di cui il metodo GetAverageSpeed tiene sempre conto invece)
+				else
+					; //TOGLI IL PUNTO E VIRGOLA //arrive_time = tempo_arrivo_prossima_stazione;		//<== questo lo ricavi cercando nella lista di eventi l'orario dell'evento di trainstop alla prossima stazione. Ti spiego come si fa
+				//int speed = getAverageSpeed((cur->GetStation()->kDistanceFromOrigin) - 5, next_station_distance, cur->GetTime(), arrive_time, cur->GetTrain());  arrive_time viene settato con il passaggio per reference
 				cur->GetTrain()->setSpeed(last_event->GetTrain()->getSpeed());
-				int fixedtime = static_cast<int>(round(static_cast<double>(distanceToPark) / cur->GetTrain()->getSpeed() * minPerHours));
-				const int delay = time - fixedtime;
-				cur->GetTrain()->editDelay(delay);
+				int fixedtime = static_cast<int>(round(static_cast<double>(abs(cur->GetStation()->kDistanceFromOrigin - next_station->kDistanceFromOrigin)) / cur->GetTrain()->getSpeed() * minPerHours));
+				const int delay = fixedtime - arrive_time;
+				cur->GetTrain()->editDelay(delay);	//Se la differenza è negativa, viene aggiunto un ritardo negativo, cioè il treno recupera ritardo 
 			}
+
+			//Ora ti manca da calcolare il tempo che ci metti per arrivare a fare la nuova richiesta
+			//La distanza da questa stazione alla prossima richiesta è di: distanza tra le stazioni (entrambi fanno la richiesta quando si trovano 20 kilometri prima
+			int time_until_request = static_cast<int>(round(static_cast<double>(abs(cur->GetStation()->kDistanceFromOrigin - next_station->kDistanceFromOrigin)) / cur->GetTrain()->getSpeed() * minPerHours));
+			Event request(cur->GetTime() + time_until_request + cur->GetTrain()->getDelay(), cur->GetTrain(), next_station, EventType::PlatformRequest);
+			events_.push_back(request);
 		}
 	}
 	
