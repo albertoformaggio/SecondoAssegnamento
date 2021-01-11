065a20abf5ec31ca42585adbe4b5a988465da7fa
Alberto Formaggio <alberto.formaggio@studenti.unipd.it>
2021-01-11 18:18:00 +0100

    Aggiornamento errori

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "controller.h"
 #include <iostream>
+#define _CRTDBG_MAP_ALLOC
+
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Controller c("line_description.txt","timetables.txt");
 	c.printEvents();
 }
